ae2c2da9a5a2d7c07b65f9f13b852c22dc0ce312
AlisoneQ <145870089+AlisoneQ@users.noreply.github.com>
2024-11-24 21:49:37 +0100

    trying canvas

--- react-app/src/components/Header.tsx
+++ react-app/src/components/Header.tsx
@@ -1,44 +1,19 @@
 function Header() {
   return (
-    <header
-      className="w-full flex flex-col items-center p-3"
-      style={{
-        background:
-          "radial-gradient(circle at top right, rgba(0, 0, 75, 0.7) 5%, rgba(0, 0, 50, 0.99))",
-        height: "35vh",
-      }}
-      onClick={(Event) => {
-        const { clientX, clientY } = Event;
-        const circle = document.createElement("div");
-        let radius = 1;
-        let fading = 2;
-        const animation = setInterval(() => {
-          radius += Math.pow(1.1, 3 - radius / 50) + 1;
-          fading -= 0.05;
-          circle.style.cssText = `
-            width: ${radius}px;
-            height: ${radius}px;
-            border: ${fading}px solid white;
-            border-radius: 100%;
-            position: absolute;
-            left: ${clientX - radius / 2}px;
-            top: ${clientY - radius / 2}px;
-            z-index: 1;
-            opacity: ${fading / 2};
-          `;
-          document.body.appendChild(circle);
-          if (fading <= 0) {
-            circle.remove();
-            clearInterval(animation);
-          }
-        }, 10);
-      }}
-    >
-      <nav
-        className="border-2 rounded-lg border-white h-fit w-fit text-l p-2 text-white fixed backdrop-blur-md flex gap-1"
-        style={{ content: "🏠" }}
-      >
-        <a href="">Home</a>
+    <header className="flex flex-col items-center">
+      <canvas
+        id="title"
+        className="w-full p-0 m-0 size-full"
+        style={{
+          background:
+            "radial-gradient(circle at top right, rgba(0, 0, 75, 0.7) 5%, rgba(0, 0, 50, 0.99), rgba(0, 0, 0, 0.9))",
+          height: "35vh",
+        }}
+      ></canvas>
+      <nav className="border-2 rounded-lg border-white h-fit w-fit text-sm p-2 text-white fixed backdrop-blur-md flex gap-1 top-0 m-2">
+        <a href="" style={{ content: "🏠" }}>
+          Home
+        </a>
       </nav>
     </header>
   );
